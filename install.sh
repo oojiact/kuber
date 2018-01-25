38dc4be1e0e2b1eab9f4d16c4d26759512809f4e
oojiact <33948432+oojiact@users.noreply.github.com>
2018-01-25 11:47:41 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
+#!/bin/sh
 apt-get update && apt-get install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF > /etc/apt/sources.list.d/kubernetes.list  
-deb http://apt.kubernetes.io/ kubernetes-xenial main  
-EOF
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb  http://apt.kubernetes.io/ kubernetes-$(lsb_release -cs) main"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
-apt-get install -y docker.io
+apt-get install -y docker-ce
 apt-get install -y kubelet kubeadm kubectl kubernetes-cni
