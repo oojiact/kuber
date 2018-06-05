b677ee4d0ff276e91a3c2d7ce84e1a438a4682a7
oojiact <33948432+oojiact@users.noreply.github.com>
2018-06-05 20:24:25 +0700

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -1,4 +1,4 @@
-exec bash
+#!/bin/sh
 setenforce 0
 sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
 #firewall-cmd --permanent --add-port=6443/tcp && firewall-cmd --permanent --add-port=2379-2380/tcp && firewall-cmd --permanent --add-port=10250/tcp && firewall-cmd --permanent --add-port=10251/tcp && firewall-cmd --permanent --add-port=10252/tcp && firewall-cmd --permanent --add-port=10255/tcp && firewall-cmd --reload
@@ -31,3 +31,7 @@ systemctl daemon-reload
 systemctl restart kubelet
 swapoff -a
 kubeadm init
+yum install -y git
+gitclone http://github.com/oojiact/kuber
+cd kuber
+kubectl apply -f cni.yaml
