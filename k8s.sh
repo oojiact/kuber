4fdfcc7e326dc7081cd664cfa7413f43dacc9874
oojiact <33948432+oojiact@users.noreply.github.com>
2018-08-09 18:18:00 +0700

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 setenforce 0
+service firewalld stop
 sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
 #firewall-cmd --permanent --add-port=6443/tcp && firewall-cmd --permanent --add-port=2379-2380/tcp && firewall-cmd --permanent --add-port=10250/tcp && firewall-cmd --permanent --add-port=10251/tcp && firewall-cmd --permanent --add-port=10252/tcp && firewall-cmd --permanent --add-port=10255/tcp && firewall-cmd --reload
 modprobe br_netfilter
