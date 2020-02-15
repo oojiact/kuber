941155180977bc1945cf07a1d89b5dee46d8679c
oojiact <33948432+oojiact@users.noreply.github.com>
2020-02-15 21:43:59 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb  http://apt.kubernetes.io/ kubernetes-$(lsb_release -cs) main"
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
-apt-get install -y docker-ce=17.03.2~ce-0~ubuntu-xenial
+#apt-get install -y docker-ce=17.03.2~ce-0~ubuntu-xenial
+apt-get install -y docker-ce
 apt-get install -y kubelet kubeadm kubectl kubernetes-cni
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
